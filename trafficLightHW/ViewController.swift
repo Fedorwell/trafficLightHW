c6c5edc728c91466803f578b51285066d2736e8e
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-04-23 02:02:23 +0300

    logic and first working version 1.0.  But small objects on screen, mb need to change  something in stack and constraints.

--- trafficLightHW/ViewController.swift
+++ trafficLightHW/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 12
+        
+        redLightView.layer.cornerRadius = redLightView.frame.size.height / 2
+        
+        yellowLightView.layer.cornerRadius = redLightView.frame.size.height / 2
+        
+        greenLightView.layer.cornerRadius = redLightView.frame.size.height / 2
+        
     }
 
-
+    @IBAction func tappedButtonToStart(_ sender: UIButton) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switchColor()
+    }
+    
+    func switchColor() {
+        counter += 1
+        if counter == 1 {
+            redLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+        } else if counter == 2 {
+            
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        } else {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            counter = 0
+        }
+    }
 }
 
